d27efe43d00298a36c7bd48c0c5b032ec6b44c1a
smilesj <smilesj@outlook.kr>
2015-10-30 00:00:12 +0900

    #A004

--- app/src/main/java/com/example/seonjae/with/SplashActivity.java
+++ app/src/main/java/com/example/seonjae/with/SplashActivity.java
@@ -7,17 +7,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
 
-public class SplashActivity extends AppCompatActivity{// Activity {
+import com.romainpiel.shimmer.Shimmer;
+import com.romainpiel.shimmer.ShimmerTextView;
+
+public class SplashActivity extends  Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        ShimmerTextView sh = (ShimmerTextView)findViewById(R.id.shimmer_tv);
+
+        Shimmer shimmer = new Shimmer();
+        shimmer.start(sh);
 
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
+
                 finish();
             }
         }, 2000);
